fab342e331eaaa1ae29dffd2b7a22a2130bbe437
ursachevlad <secretariat.studentium@gmail.com>
2017-03-20 23:47:45 +0200

    max - min numar

---
+++ src/afisaremaxim.java
@@ -0,0 +1,17 @@
+
+public class afisaremaxim {
+    public static void main(String[ ] args) {
+        int res = max(7, 42);
+        System.out.println(res); //42
+    }
+
+    static int max(int a, int b) {
+        if(a > b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+
+}
