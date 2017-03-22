774162b13230910b6b76f6b10fe177e0bf6a99e3
ursachevlad <secretariat.studentium@gmail.com>
2017-03-22 21:00:50 +0200

    calcul factorial

---
+++ src/calcFactorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class calcFactorial {
+
+        public static void main(String args[])
+        {
+            int n, c, fact = 1;
+
+            System.out.println("Introdu un numar pentru a calcula factorialul acestuia");
+            Scanner in = new Scanner(System.in);
+
+            n = in.nextInt();
+
+            if ( n < 0 )
+                System.out.println("Numarul nu ar trebui sa fie negativ");
+            else
+            {
+                for ( c = 1 ; c <= n ; c++ )
+                    fact = fact*c;
+
+                System.out.println("Factorial din "+n+" este = "+fact);
+            }
+        }
+    }
